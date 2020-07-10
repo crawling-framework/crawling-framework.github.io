2e0584adb9e5cad52ab8bed574dc0bb641d75596
Lukyanov Kirill <lukyanov_kirya@bk.ru>
2020-07-10 12:25:25 +0300

    test code KNN-UCB v1

--- src/crawlers/KNN-UCB.py
+++ src/crawlers/KNN-UCB.py
@@ -18,50 +18,123 @@ class KNN_UCB_Crawler(Crawler):
         initial_seed = self._orig_graph.random_node()
         self.observed_set.add(initial_seed)
 
+        dct_observed = dict()
+        dct_crawled = dict()
 
         while(len(self.observed_set) > 0):
 
             best_vert = -1
-            expected_gain = -1
+            best_expected_gain = -1
 
-            dct = dict()
+            for i in range(self.crawled_set):
+
+                if dct_crawled.get(i) is not None:
+                    # if dct_crawled.get(i)[1]:
+                    if 0 == 0:
+                        vert_degree = MyGraph.deg(self, i)
+                        max_neb_degree = 0
+                        aver_neb_degree = 0
+                        kol_craw_neb = 0
+                        for j in MyGraph.neighbors(self, i):
+                            if j in self.crawled_set:
+                                aver_neb_degree += MyGraph.deg(self, j)
+                                kol_craw_neb += 1
+                                if MyGraph.deg(self, j) > max_neb_degree:
+                                    max_neb_degree = MyGraph.deg(self, j)
+
+                        aver_neb_degree /= kol_craw_neb
+                        aver_neb_degree = round(aver_neb_degree)
+
+                        # !!!!!!!!!!!!!!!!!!!!!!!!!!!
+                        # вычислить метрику для треугольников
+                        # kol_triangl = 0
+                        # !!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                        vec = np.array((vert_degree, max_neb_degree, aver_neb_degree))
+                        # vec = np.array((vert_degree, max_neb_degree, aver_neb_degree, kol_triangl))
+                        dct_crawled.update({i, (vec, False, dct_crawled.get(i)[2])})
+
 
             for i in range(self.observed_set):
+                if dct_observed.get(i) is not None:
+                    # if dct_observed.get(i)[1]:
+                    if 0 == 0:
+                        vert_degree = MyGraph.deg(self, i)
+                        max_neb_degree = 0
+                        aver_neb_degree = 0
+                        kol_craw_neb = 0
+                        for j in MyGraph.neighbors(self, i):
+                            if j in self.crawled_set:
+                                aver_neb_degree += MyGraph.deg(self, j)
+                                kol_craw_neb += 1
+                                if MyGraph.deg(self, j) > max_neb_degree:
+                                    max_neb_degree = MyGraph.deg(self, j)
 
-                if dct.get(i) is not None:
-                    if dct.get(i)[1]:
+                        aver_neb_degree /= kol_craw_neb
+                        aver_neb_degree = round(aver_neb_degree)
+
                         # !!!!!!!!!!!!!!!!!!!!!!!!!!!
-                        # вычислить метрики для вершины
-                        vert_degree = 1
-                        max_neb_degree = 10
-                        aver_neb_degree = 5
-                        kol_triangl = 0
+                        # вычислить метрику для треугольников
+                        # kol_triangl = 0
                         # !!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-                        vec = np.array((vert_degree, max_neb_degree, aver_neb_degree, kol_triangl))
-                        dct.update({i, (vec, False)})
+                        vec = np.array((vert_degree, max_neb_degree, aver_neb_degree))
+                        # vec = np.array((vert_degree, max_neb_degree, aver_neb_degree, kol_triangl))
+                        dct_observed.update({i, (vec, False)})
 
-            for i in range(self.observed_set):
+            for key, value in dct_observed:
                 arr = []
-                for key, value in dct:
-                    if key != i:
-                        arr.append((key, self.count_dist(value[0], dct.get(i)[0])))
+                for i in range(self.crawled_set):
+                    arr.append((key, self.count_dist(value[0], dct_crawled.get(i)[0])))
                 arr.sort(key=lambda x: x[1])
 
                 f = 0
                 sigm = 0
 
-                for j in range(k):
-                    # !!!!!!!!!!!!!!!!!!!
-                    # f += y[j] / arr[j][1]
-                    # !!!!!!!!!!!!!!!!!!!!!!!!!!
-                    f += 1 / (k * arr[j][1] + 1)  # <-------- +1
-                    sigm += arr[j][1] / k
+                if len(self.crawled_set) >= k:
+                    flag = 0
+                    kol = 0
+                    for j in range(k):
+                        if arr[j][1] == 0:
+                            flag = 1
+                            kol += 1
+                            f += dct_crawled.get(j)[2]
+                            sigm += arr[j][1]
+                        elif flag == 0:
+                            f += dct_crawled.get(j)[2] / arr[j][1]
+                            sigm += arr[j][1]
+                    if flag == 0:
+                        f /= k
+                        sigm /= k
+                    else:
+                        f /= kol
+                        sigm /= kol
+                else:
+                    kol = 0
+                    flag = 0
+                    for j in range(len(self.crawled_set)):
+                        if arr[j][1] == 0:
+                            flag = 1
+                            kol += 1
+                            f += dct_crawled.get(j)[2]
+                            sigm += arr[j][1]
+                        elif flag == 0:
+                            f += dct_crawled.get(j)[2] / arr[j][1]
+                            sigm += arr[j][1]
+                    f /= kol
+                    sigm /= kol
 
-                if(f + alf * 1 > expected_gain):  # <--------- !!!!!!!!!!!!!
-                    expected_gain = f + alf * 1
-                    best_vert = i
+                if(f + alf * sigm > best_expected_gain):
+                    best_expected_gain = f + alf * sigm
+                    best_vert = key
 
+            y = 0
+            for j in MyGraph.neighbors(self, best_vert):
+                if not(j in self.crawled_set or j in self.observed_set):
+                    y += 1
+            dct_crawled.update({best_vert, (np.array((1, 1, 1)), False, y)})
+
+            dct_observed.pop(best_vert)
             self.crawl(best_vert)
 
-        return self.crawlers
+        return self.crawled_set
