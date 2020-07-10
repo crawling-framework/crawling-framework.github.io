3a70b2b7f98ffab44acaba87010e0c07d974afcc
mishadr <aytm@mail.ru>
2020-07-10 13:40:51 +0300

    +

---
+++ src/experiments/mab.py
@@ -0,0 +1,35 @@
+import logging
+
+from crawlers.cbasic import MaximumObservedDegreeCrawler
+from graph_io import GraphCollections
+from crawlers.knn_ucb import KNN_UCB_Crawler
+from running.animated_runner import AnimatedCrawlerRunner
+from running.metrics_and_runner import TopCentralityMetric
+from statistics import Stat
+
+
+def test_knnucb():
+    g = GraphCollections.get('dolphins')
+    # g = GraphCollections.get('Pokec')
+
+    p = 1
+    # budget = int(0.005 * g.nodes())
+    # s = int(budget / 2)
+
+    crawler_defs = [
+        # (KNN_UCB_Crawler, {}),
+        (MaximumObservedDegreeCrawler, {}),
+    ]
+    metric_defs = [
+        (TopCentralityMetric, {'top': p, 'centrality': Stat.DEGREE_DISTR.short, 'measure': 'Re', 'part': 'nodes'}),
+    ]
+    acr = AnimatedCrawlerRunner(g, crawler_defs, metric_defs, budget=100)
+    acr.run()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(message)s')
+    logging.getLogger('matplotlib.font_manager').setLevel(logging.INFO)
+    # logging.getLogger().setLevel(logging.DEBUG)
+
+    test_knnucb()
